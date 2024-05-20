cd5f06fb558da57ab256d5c5f3044997da4bc07e
pablo_sifuentes <sifuentespablo22@gmail.com>
2024-05-20 19:57:19 -0300

    Criação do Projeto da SA.

---
+++ src/main/java/br/com/senai/sa2semestre/suportemanutencao/SuporteManutencaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.sa2semestre.suportemanutencao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuporteManutencaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuporteManutencaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senai/sa2semestre/suportemanutencao/SuporteManutencaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.sa2semestre.suportemanutencao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuporteManutencaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
